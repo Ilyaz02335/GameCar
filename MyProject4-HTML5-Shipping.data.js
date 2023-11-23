876eef25ba34e1c799bbb378052ac9aeaa04cd2d
Ilyaz02335 <147247680+Ilyaz02335@users.noreply.github.com>
2023-11-23 15:43:50 +0700

    Update MyProject4-HTML5-Shipping.data.js

--- MyProject4-HTML5-Shipping.data.js
+++ MyProject4-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'D:/projects/MyProject4/Binaries/HTML5/MyProject4-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'MyProject4-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/scl/fi/fb68jh1w1e0ewnp6bukxb/MyProject4-HTML5-Shipping.data?rlkey=dsudvrp16jq6y0ovlw4hytme9&dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/scl/fi/fb68jh1w1e0ewnp6bukxb/MyProject4-HTML5-Shipping.data?rlkey=dsudvrp16jq6y0ovlw4hytme9&dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
